9e9ed896376e18abaced22d4b17fcc2322a109f5
Ricardo Olarte <raop155@hotmail.com>
2021-01-12 12:33:52 -0500

    Add fethReducerCreator function

--- utils/redux.js
+++ utils/redux.js
@@ -19,55 +19,63 @@ const createReducer = (iS, handlers) => (state = iS, action) => {
   }
 };
 
-const t = makeType('Todos');
-const FETCH_START = t('FETCH_START');
-const FETCH_SUCCESS = t('FETCH_SUCCESS');
-const FETCH_ERROR = t('FETCH_ERROR');
+const fetchReducerCreator = (resource) => {
+  const t = makeType(resource);
+  const FETCH_START = t('FETCH_START');
+  const FETCH_SUCCESS = t('FETCH_SUCCESS');
+  const FETCH_ERROR = t('FETCH_ERROR');
 
-const initialState = {
-  data: [],
-  fetched: false,
-  fetching: false,
-  error: '',
+  const initialState = {
+    data: [],
+    fetched: false,
+    fetching: false,
+    error: '',
+  };
+
+  const fetchStartReduce = (state) => ({
+    ...state,
+    fetching: true,
+  });
+
+  const fetchSuccessReduce = (state, action) => ({
+    ...state,
+    fetching: false,
+    fetched: true,
+    data: action.payload,
+  });
+
+  const fetchErrorReduce = (state, action) => ({
+    ...state,
+    fetching: false,
+    error: action.error,
+  });
+
+  const reducer = createReducer(initialState, {
+    [FETCH_START]: fetchStartReduce,
+    [FETCH_SUCCESS]: fetchSuccessReduce,
+    [FETCH_ERROR]: fetchErrorReduce,
+  });
+
+  const startFetch = makeActionCreator(FETCH_START);
+  const successFetch = makeActionCreator(FETCH_SUCCESS, 'payload');
+  const errorFetch = makeActionCreator(FETCH_ERROR, 'error');
+
+  return {
+    reducer,
+    fetch: () => async (dispatch) => {
+      dispatch(startFetch());
+
+      try {
+        const response = await fetch(`/${resource}`);
+        const data = await response.json();
+        dispatch(successFetch(data));
+      } catch (error) {
+        dispatch(errorFetch(error));
+      }
+    },
+  };
 };
 
-const fetchStartReduce = (state, _) => ({
-  ...state,
-  fetching: true,
-});
-
-const fetchSuccessReduce = (state, action) => ({
-  ...state,
-  fetching: false,
-  fetched: true,
-  data: action.payload,
-});
-
-const fetchErrorReduce = (state, action) => ({
-  ...state,
-  fetching: false,
-  error: action.error,
-});
-
-export default (initialState,
-{
-  [FETCH_START]: fetchStartReduce,
-  [FETCH_SUCCESS]: fetchSuccessReduce,
-  [FETCH_ERROR]: fetchErrorReduce,
-});
-
-const startFetch = makeActionCreator(FETCH_START);
-const successFetch = makeActionCreator(FETCH_SUCCESS, 'payload');
-const errorFetch = makeActionCreator(FETCH_ERROR, 'error');
-
-export const fetch = () => async (dispatch, _) => {
-  dispatch(startFetch());
-
-  try {
-    const response = await fetch('/todos');
-    const data = await response.json();
-    dispatch(successFetch(data));
-  } catch (error) {
-    dispatch(errorFetch(error));
-  }
-};
+const createdReducer = fetchReducerCreator('todos');
+export default createdReducer;
+export const fetch = createdReducer.fetch;
